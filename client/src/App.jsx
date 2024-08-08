f87fd7ca4fab0f0b5ff959e499d93ce9c04e902c
Derrick Korir <derrickroz4227@gmail.com>
2024-08-08 11:47:57 +0300

    logins complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,20 @@
-import Navbar from './components/Navbar'; // Adjust the path if needed
-import ProfileDashboard from './components/ProfileDashboard';
-import HistoryPage from './pages/HistoryPage';
-import PersonalInfoPage from './pages/PersonalInfoPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import HomePage from './components/Home';
-import EventListing from './components/EventsList';
+// import ProfileDashboard from './pages/ProfileDashboard'; // Ensure this is correct
+
 const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
         <Routes>
-            <Route path="/" element={<HomePage />} />
-          {/* <Route path="/home" element={<HomePage />} /> */}
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/profile/*" element={<ProfileDashboard />} />
-          <Route path="/profile/personal-info" element={<PersonalInfoPage />} />
-          <Route path="/profile/history" element={<HistoryPage />} />
-          <Route path="/events" element={<EventListing />} />
+          {/* <Route path="/profile/*" element={<ProfileDashboard />} /> */}
         </Routes>
       </div>
     </Router>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/events">Events</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
+        
       </ul>
     </nav>
   );
