0cfab5b70e922f528dcc5acfaed5eac0bfcc30f9
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-08-10 19:14:26 +0300

    about us page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import { router } from "../route";
+import { router } from "./route";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,14 +1,56 @@
-function Aboutus() {
+import Card from "react-bootstrap/Card";
+import PageNavigation from "../components/PageNavigation";
+import "../AboutUs.css";
+
+function AboutUs() {
   return (
-    <div
-      style={{
-        background:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fphotos.com%2Ffeatured%2Fblue-and-orange-fireworks-at-night-over-karenmassier.html&psig=AOvVaw0zCHm8Eg0eeoX2SxZ0ej18&ust=1723288854360000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIigj7jl54cDFQAAAAAdAAAAABAE",
-      }}
-    >
-      <p>HELLO WORLD</p>
-    </div>
+    <>
+      <Card className="bg-dark text-white">
+        <Card.Img
+          src="https://www.firstevent.co.uk/wp-content/uploads/2023/10/AIBlog.png"
+          alt="Card image"
+          style={{ width: "100%", height: "100vh" }}
+        />
+
+        <Card.ImgOverlay>
+          <PageNavigation />
+          <br />
+          <div className="aboutus">
+            <Card.Title>WELCOME TO THE TOP EVENTS APP!!</Card.Title>
+
+            <div className="aboutus-text">
+              <Card.Text>
+                Welcome to Epic Events.This is your one-stop-shop for all your
+                event ticketing needs. Our app provides a seamless way for users
+                to discover, buy, and sell tickets to upcoming events. Whether
+                you're an event organizer looking to sell tickets to your event
+                or an attendee searching for the next big thing, our app has got
+                you covered. Easy Ticket Buying With Epic Events, buying tickets
+                to your favorite events has never been easier. Browse through
+                our extensive list of upcoming events, select your desired
+                event, and purchase tickets in just a few taps. Our app ensures
+                a secure and hassle-free transaction process, so you can focus
+                on what matters most - enjoying the event! Add Your Own Event
+                Are you an event organizer looking to promote your event and
+                sell tickets? Look no further! Our app allows you to easily add
+                your own event, set ticket prices, and track sales in real-time.
+                With our user-friendly interface, you can create an event
+                listing in minutes and start selling tickets right away. Why
+                Choose Epic Events ?Low Ticket Fees: We offer competitive ticket
+                fees, ensuring that you get the best value for your money. Easy
+                Event Management: Our app provides a simple and intuitive way to
+                manage your events, from creating listings to tracking sales.
+                Secure Transactions: Our app ensures a secure and reliable
+                transaction process, so you can focus on enjoying the event. Get
+                Started Today! Download Epic Events now and start discovering,
+                buying, and selling tickets to upcoming
+              </Card.Text>
+            </div>
+          </div>
+        </Card.ImgOverlay>
+      </Card>
+    </>
   );
 }
 
-export default Aboutus;
+export default AboutUs;
